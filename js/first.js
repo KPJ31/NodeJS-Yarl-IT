8e48fab358adbeac9fadb94fbde5f8bf7bbd212d
K.P.Janarthan <90019406+KPJ31@users.noreply.github.com>
2026-04-29 10:05:47 +0530

    28/04/2026

---
+++ js/first.js
@@ -0,0 +1,30 @@
+// console.log("Wellcome to JavaScript");
+// console.log("Wellcome to JavaScript");
+// console.log("Wellcome to JavaScript");
+
+
+var x = 10;
+console.log(x);
+
+let y = 20;
+console.log(y);
+
+// let y = 30;
+// console.log(y);
+
+var x = 30.03;
+console.log(x);
+
+y = 50;
+console.log(y);
+
+console.log(typeof y);
+console.log(typeof x);
+
+
+name = 'jana'
+fname = "panchalingam"
+lname = `janarthan` // string templet
+console.log(typeof fname);
+
+console.log("my name is " + fname + ' ' + lname);
